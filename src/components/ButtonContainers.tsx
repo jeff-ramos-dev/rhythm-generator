3568b9ba4db86f6962072fc48c7010c657b9f930
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-08-30 20:35:21 -0700

    WIP extract buttons component

---
+++ src/components/ButtonContainers.tsx
@@ -0,0 +1,55 @@
+type buttonProps = {
+    handleClick: Function;
+    handleSetting: Function;
+    changeDifficulty: Function;
+    difficultyLevels: string[];
+    state: string;
+}
+
+export default function ButtonContainers(props: buttonProps) {
+    const {handleClick, handleSetting, changeDifficulty, difficultyLevels, state} = props
+    return (
+        <>
+            <div className="buttonContainer">
+                <button 
+                    className="new" 
+                    onClick={(event) => handleClick}
+                >
+                    New {state} 
+                </button>
+                <button 
+                    className="setting" 
+                    onClick={(event) => handleSetting}
+                >
+                    Switch to {state === 'phrase' ? 'Bar' : 'Phrase'} 
+                </button>
+            </div>
+            <div className="buttonContainer">
+                <button 
+                    className={`difficulty ${difficultyLevels[0]}`} 
+                    onClick={(event) => changeDifficulty(event)}
+                >
+                    mild
+                </button>
+                <button 
+                    className={`difficulty ${difficultyLevels[1]}`} 
+                    onClick={(event) => changeDifficulty(event)}
+                >
+                    medium
+                </button>
+                <button 
+                    className={`difficulty ${difficultyLevels[2]}`} 
+                    onClick={(event) => changeDifficulty(event)}
+                >
+                    hot
+                </button>
+                <button 
+                    className={`difficulty ${difficultyLevels[3]}`} 
+                    onClick={(event) => changeDifficulty(event)}
+                >
+                    fire
+                </button>
+            </div>
+        </>
+    )
+}
